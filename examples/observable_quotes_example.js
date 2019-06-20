0553212083a01b1c2638ce96ac42593a7468675f
jspenc72 <jspenc72@gmail.com>
2019-06-19 20:28:28 -0600

    Update observable_quotes_example.js

--- examples/observable_quotes_example.js
+++ examples/observable_quotes_example.js
@@ -1,7 +1,16 @@
-var credentials = {
-    username: '',
-    password: '',
-};
+var credentials;
+
+if (process.env.ROBINHOOD_USERNAME) {
+  // No need to store credentials in file or code, robinhood_observer will check for env vars if none are provided
+  // See docker-compose starter https://github.com/jspenc72/robinhood-observer-starter 
+  credentials = null
+}else{
+  credentials = {
+    username: process.env.ROBINHOOD_USERNAME || '',
+    password: process.env.ROBINHOOD_PASSWORD || '',
+  };
+}
+
 
 var Robinhood = require('../src')(credentials, function(){
     var subscription = Robinhood.observeQuote(['TVIX', 'AAPL', 'GOOG'])
